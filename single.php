0f6a9f92d9bd0c0ae667e2201b74d07640e7509e
laras126 <laras126@gmail.com>
2015-11-04 12:59:55 -0500

    add taxonomy arg to piece loop query

--- single.php
+++ single.php
@@ -19,9 +19,19 @@ $context['comment_form'] = TimberHelper::get_comment_form();
 
 // Adding some logic here - may be better in single-piece.php, but okay for now unless it gets more extensive.
 if ( get_post_type() == 'piece') {
+	$terms = get_the_terms($post->ID, 'collection');
 	$piece_args = array(
 		'post_type' => 'piece',
-		'post__not_in' => array( $post->ID ) 
+		'post__not_in' => array( $post->ID ),
+		'posts_per_page' => -1,
+		'tax_query' => array(
+	        array(
+	            'taxonomy' => 'collection', 
+	            'terms' => $terms[0],
+	            'field' => 'id', 
+	            'operator' => 'IN'
+	        )
+	    )
 	);
 	$context['pieces'] = Timber::get_posts($piece_args);
 	$context['concierge_text'] = get_field('concierge_text', 'options');
