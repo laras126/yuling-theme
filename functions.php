702cc703dc001e0167bd1f98a58c2e7d30bdc3de
laras126 <laras126@gmail.com>
2015-11-19 16:54:37 -0500

    fix prod min css path

--- functions.php
+++ functions.php
@@ -104,7 +104,7 @@
 		// Enqueue stylesheet and scripts. Use minified for production.
 		// NOTE: will need to change this to get_stylesheet_directory_uri() to allow for child themes later.
 		if( WP_ENV == 'production' ) {
-			wp_enqueue_style( 'tsk-styles', get_stylesheet_directory_uri() . '/assets/css/build/main.min.css', 1.0);
+			wp_enqueue_style( 'tsk-styles', get_stylesheet_directory_uri() . '/assets/css/main.min.css', 1.0);
 			wp_enqueue_script( 'tsk-js', get_stylesheet_directory_uri() . '/assets/js/build/scripts.min.js', array('jquery'), '1.0.0', true );
 		} else {
 			wp_enqueue_style( 'tsk-styles', get_stylesheet_directory_uri() . '/assets/css/main.css', 1.0);
