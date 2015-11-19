5549985856ad75b1d6060279b2cb2ce335d29066
laras126 <laras126@gmail.com>
2015-11-19 16:35:45 -0500

    add username to sidebar right context

--- sidebar-footer_right.php
+++ sidebar-footer_right.php
@@ -8,5 +8,10 @@
  */
 
 $data['widgets'] = Timber::get_widgets('footer_right');
+// Social
+$data['pinterest'] = get_field('pinterest', 'options');
+$data['twitter'] = get_field('twitter', 'options');
+$data['facebook'] = get_field('facebook', 'options');
+$data['instagram'] = get_field('instagram', 'options');
 
 Timber::render(array('sidebars/sidebar-footer_right.twig'), $data);
