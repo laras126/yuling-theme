e3f2735e68f8dcd1dd355cd3d593694df42bc313
laras126 <laras126@gmail.com>
2015-11-19 16:13:43 -0500

    add all collections template and view

---
+++ page-collections.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Template Name: Collections
+ * Description: The template to show an archive of all collections.
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['terms'] = Timber::get_terms('collection');
+Timber::render('pages/page-collections.twig', $context);
