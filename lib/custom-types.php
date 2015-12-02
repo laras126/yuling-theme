8e9292ce95151a027489d06a701872b3c408bc27
laras126 <laras126@gmail.com>
2015-12-02 12:02:10 -0500

    remove excess from custom types and add new reqwrite arg

--- lib/custom-types.php
+++ lib/custom-types.php
@@ -1,56 +1,5 @@
 <?php 
 
-	// ----
-	// Pieces Custom Type
-	// ----
-
-	$labels = array(
-		'name'                => _x( 'Collection', 'Post Type General Name', 'yld' ),
-		'singular_name'       => _x( 'Collection', 'Post Type Singular Name', 'yld' ),
-		'menu_name'           => __( 'Collections', 'yld' ),
-		'name_admin_bar'      => __( 'Collection', 'yld' ),
-		'parent_item_colon'   => __( 'Parent Collection:', 'yld' ),
-		'all_items'           => __( 'All Collections', 'yld' ),
-		'add_new_item'        => __( 'Add New Collection', 'yld' ),
-		'add_new'             => __( 'Add New', 'yld' ),
-		'new_item'            => __( 'New Collection', 'yld' ),
-		'edit_item'           => __( 'Edit Collection', 'yld' ),
-		'update_item'         => __( 'Update Collection', 'yld' ),
-		'view_item'           => __( 'View Collection', 'yld' ),
-		'search_items'        => __( 'Search Collection', 'yld' ),
-		'not_found'           => __( 'Not found', 'yld' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'yld' ),
-	);
-	$rewrite = array(
-		'slug'                => 'collections',
-		'with_front'          => true,
-		'pages'               => true,
-		'feeds'               => true,
-	);
-	$args = array(
-		'label'               => __( 'collection_type', 'yld' ),
-		'description'         => __( 'The Collection post type.', 'yld' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'excerpt', 'thumbnail', 'revisions' ),
-		'taxonomies'          => array( 'category', 'post_tag' ),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'menu_position'       => 5,
-		'menu_icon'           => 'dashicons-format-gallery',
-		'show_in_admin_bar'   => true,
-		'show_in_nav_menus'   => true,
-		'can_export'          => true,
-		'has_archive'         => true,		
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'rewrite'             => $rewrite,
-		'capability_type'     => 'page',
-	);
-	// register_post_type( 'collection_type', $args );
- 
-
 
 	// ----
 	// Pieces Custom Type
@@ -74,7 +23,7 @@
 		'not_found_in_trash'  => __( 'Not found in Trash', 'yld' ),
 	);
 	$rewrite = array(
-		'slug'                => 'pieces',
+		'slug'                => 'collection/%collection%',
 		'with_front'          => true,
 		'pages'               => true,
 		'feeds'               => true,
