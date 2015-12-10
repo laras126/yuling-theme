80fba625f4614df87e1fc7ee7696fce8f7a63a5c
laras126 <laras126@gmail.com>
2015-12-10 10:38:46 -0500

    clean up spacing and remove form context var from single

--- single.php
+++ single.php
@@ -10,6 +10,7 @@
  */
 
 
+
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
@@ -19,6 +20,7 @@ $context['comment_form'] = TimberHelper::get_comment_form();
 
 // Adding some logic here - may be better in single-piece.php, but okay for now unless it gets more extensive.
 if ( get_post_type() == 'piece') {
+
 	$terms = get_the_terms($post->ID, 'collection');
 	$piece_args = array(
 		'post_type' => 'piece',
@@ -32,16 +34,11 @@ if ( get_post_type() == 'piece') {
 	            'operator' => 'IN'
 	        )
 	    )
-	);
+	);	
+
 	$context['pieces'] = Timber::get_posts($piece_args);
 	$context['concierge_text'] = get_field('concierge_text', 'options');
-	// $context['form'] = FrmAppController::get_form_shortcode(array('id' => '4', 'key' => '', 'title' => false, 'description' => false, 'readonly' => false, 'entry_id' => false, 'fields' => '14'));
-	$context['form'] = FrmFormsController::get_form_shortcode( 
-		array( 
-			'id' => 4, 
-			'title' => false, 
-			'description' => false
-		) );
+	
 }
 
 if (post_password_required($post->ID)){
