523d20247eb9a32f6bc38c60104f63346d436640
laras126 <laras126@gmail.com>
2015-12-13 11:51:04 -0500

    add form to context

--- page-concierge.php
+++ page-concierge.php
@@ -6,10 +6,10 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-// $context['form'] = FrmFormsController::get_form_shortcode( 
-// 		array( 
-// 			'id' => 3, 
-// 			'title' => false, 
-// 			'description' => false
-// 		) );
+$context['form'] = FrmFormsController::get_form_shortcode( 
+		array( 
+			'id' => 3, 
+			'title' => false, 
+			'description' => false
+		) );
 Timber::render(array('pages/page-concierge.twig', 'pages/page.twig'), $context);
