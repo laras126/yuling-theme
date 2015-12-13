401acc65d996285eccecba0ca5d57535c619c772
laras126 <laras126@gmail.com>
2015-12-13 15:02:50 -0500

    removal working, starting on inline editing

--- assets/js/src/_main.js
+++ assets/js/src/_main.js
@@ -217,19 +217,22 @@ $(document).ready(function() {
 
 		updateWishListCount();
 
+		// If on a Single Piece page with associated variable made available by WordPress
 		if (typeof php_vars !== 'undefined') { 
+			
 			isPiece = true;
 			
-			// TODO: consolidate this stuff and not have so many ifs.
+			// On a Single Piece page with php_vars defined
 			if (isPiece === true) {
 
-				// If you aren't on a single piece page, define PHP vars.
-				// TODO: there's definitely a more elegant solution for this...
-
+				// Get the current piece item
 				var currentItem = window.localStorage.getItem('wishListItem_' + php_vars.id);
 
+				// Populate the hidden fields with piece and collection names
+				$('#pieceName').val(php_vars.title);
+				$('#pieceCollection').val(php_vars.collection[0].name);
+				
 				// Store piece number and name on submit
-				// TODO: AJAX this
 				$('#wishListForm').on('submit', function(e) {
 					
 					var piece = {
@@ -238,70 +241,231 @@ $(document).ready(function() {
 						'collection': php_vars.collection[0].name
 					};
 
+					// Update piece quantity in dropdown
 					piece.quantity = $('#quantity option:selected').val();
+
+					// Set the value in localStorage
 					window.localStorage.setItem('wishListItem_' + piece.id, JSON.stringify(piece));
-					var updatedObj = JSON.parse(window.localStorage.getItem('wishListItem_' + piece.id));
-					console.log(updatedObj);
 					
+					// UI Notification
 					$('#wishListNotify').html('Updated').animate({opacity:1}, 300);
 
-					updatewishListCount();
+					// Update the Wish List number in the header
+					updateWishListCount();
+
+					// Do not refresh page
 					e.preventDefault();
 				});
 
-
-				// Populate the hidden fields with piece and collection names
-				$('#pieceName').val(php_vars.title);
-				$('#pieceCollection').val(php_vars.collection[0].name);
-				
-				
-				// console.log(retrievedObj.quantity);
-				// console.log(typeof retrievedObj);
-				if ( currentItem === null) {
-					console.log('poop');
-				} else {
-					// TODO: need to account for quantity undefined - not sure why this logic isn't working.
+				if ( currentItem !== null) {
+					// If current item is in the Wish List, update it's status
 					var retrievedObj = JSON.parse(window.localStorage.getItem('wishListItem_' + php_vars.id));
-
 					markActiveWishList(retrievedObj);
-					console.log(retrievedObj);
 
 				}
 			} // END isPiece logic
 		} // END php_vars logic
+
+
+
+
+		// If there are Wish List items
+		if (window.localStorage.length != 0) {
+			
+			for(var i in window.localStorage) {
+				var item = JSON.parse(window.localStorage[i]);
+				populateWishListTable(item);
+			}
+
+			// $('#wishListItems').on('click', function() {
+			// 	$('.save-changes').fadeIn(300);
+			// });
+
+			$('#saveChangesBtn').on('click', function() {
+				// console.log('hi');
+				
+			});
+
+			// Update or remove quantity value
+			$('.edit-label').on('click', function(e) {
+				var prevValue = $(this).closest('tr').find('.quantity-value').html();
+				
+				// Populate input with current quantity
+				$(this).closest('tr')
+						.find('.quantity-value')
+						// .html('<input type="number" class="edit-quantity-field" value="' + prevValue + '"><span id="updateQuantity">Update</span>');
+						.html('<input type="number" class="edit-quantity-field" value="' + prevValue + '">');
+
+				$('#saveChanges').fadeIn(300);
+
+				// Hide the current quantity
+				// $(this).closest('tr')
+				// 	.find('.quantity-value')
+				// 	.hide();
+
+				// // Show the editing options
+				// var editingHTML = $(this).closest('tr')
+				// 	.find('#isEditing').html();
+
+				// $(this).closest('tr')
+				// 	.find('.edit-quantity-form')
+				// 	.html(editingHTML);
+
+				e.preventDefault();
+			});
+
+
+			// $('#updateQuantity').on('click', function() {
+			// 	var targetID = $(this).closest('form').attr('id');
+			// 	var targetEntry = JSON.parse(window.localStorage.getItem(targetID));
+			// 	targetEntry.quantity = $('.edit-quantity-field').val();
+				
+			// 	console.log('targetEntry'); 
+				
+			// });
+
+
+			$('.remove-label').on('click', function(e) {
+				var targetID = $(this).closest('form').attr('id');
+				var targetEntry = JSON.parse(window.localStorage.getItem(targetID));
+
+				// Remove item from localStorage
+				window.localStorage.removeItem(targetID);					
+				
+				// Update the UI to indicate row removed
+				$(this).closest('tr').addClass('row-removed');
+
+				// Update the value of hidden textarea and header count
+				updateTexarea();
+				updateWishListCount();
+
+				console.log(window.localStorage);
+				
+				e.preventDefault();
+			});
+
+			// $('.expects-save').on('click', function(e) {
+			// 	var targetID = $(this).closest('form').attr('id');
+			// 	var targetEntry = window.localStorage.getItem(targetID);
+
+
+			// 	$(this).html('Edit').attr('class', 'edit-label');
+			// 	;
+
+			// 	// $(this).closest('tr').find('.quantity-value').css('background','green');
+			// 	console.log('saved');
+
+			// 	e.preventDefault();
+			// });
+
+		} // END Wish List length check
+
 	} // END localStorage check
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---
+// Update the Wish List count in header
+// ---
 
 function updateWishListCount() {
 
 	if (window.localStorage.length != 0) {
 		$('#wishListCount').html(window.localStorage.length);
+	}
+
+}
+
+
+
+// ---
+// Populate the Wish List table
+// ---
+
+function populateWishListTable(item) {
+	if (window.localStorage.length != 0) {
 		
-		for(var i in window.localStorage) {
-			var item = JSON.parse(window.localStorage[i]);
-			var title = '<td class="item-title">' + item.title + '</td>',
-				collection = '<td class="item-collection">' + item.collection + '</td>',
-				quantity = '<td class="item-quantity">' + item.quantity + '</td>';
+		// Markup for editing the item's quantity or removing it from localStorage
+		var editFormMarkup = '<form class="edit-quantity-form" id="wishListItem_' + item.id + '">';
 
-			// Update the Wish List header text to indicate there are items
-			$('#wishListTitle').html('In Your Wish List');
-			$('#wishListPrompt').html('Complete the email form below to receive a price quote for these items.');
+			// Edit Radio
+			editFormMarkup += '<input type="radio" name="edit_quantity_' + item.id + '" id="editRadio_' + item.id + '">';
+			editFormMarkup += '<label class="edit-label" for="editRadio_' + item.id + '">Edit</label>'
 
-			// Add item to table
-			$('#wishListItems').append('<tr>' + collection + title + quantity + '</tr>');
+			// Remove Radio
+			editFormMarkup += '<input name="edit_quantity_' + item.id + '" id="removeRadio_' + item.id +'" type="radio">';
+			editFormMarkup += '<label class="remove-label" for="removeRadio_' + item.id +'">Remove</label></form>';
+		
 
-			// Fill hidden Wish List text area with content
-			$('.wishlist-fill textarea').append(item.collection + ': ' + item.title + ', ' + item.quantity + ' || ');
-		}
+		// var isEditingMarkup = '<span id="isEditing"><input type="number" class="edit-quantity-field" value="">';
+		// 	isEditingMarkup += '<span id="updateValue" class="editing-update">Update</span> <span id="cancelUpdate" class="editing-cancel">Cancel</span></span>';
+				
+
+		var title = '<td class="item-title">' + item.title + '</td>',
+			collection = '<td class="item-collection">' + item.collection + '</td>',
+			quantity = '<td class="item-quantity"><span class="quantity-value">' + item.quantity + '</span>' + editFormMarkup + '</td>';
+
+		// Update the Wish List header text to indicate there are items
+		$('#wishListTitle').html('In Your Wish List');
+		$('#wishListPrompt').html('Complete the email form below to receive a price quote for these items.');
+
+		// Add item to table
+		$('#wishListItems').append('<tr>' + collection + title + quantity + '</tr>');
+
+		// Fill hidden Wish List text area with content
+		$('.wishlist-fill textarea').append(item.collection + ': ' + item.title + ', ' + item.quantity + ' || ');
+
 	} else {
-		
-		// Remove the table if there are no items in the List
 		$('#wishListItems').remove();
 	}
 }
+
+
+
+// ---
+// Update the hidden text area to record values on form submission
+// ---
+
+function updateTexarea() {
+	$('.wishlist-fill textarea').html('');
+	for(var i in window.localStorage) {
+		var item = JSON.parse(window.localStorage[i]);
+		$('.wishlist-fill textarea').append(item.collection + ': ' + item.title + ', ' + item.quantity + ' || ');
+	}
+}
+
+
+
+// ---
+// Update the Wish List UI when piece quantity is changed
+// ---
 
 function markActiveWishList(obj) {
 	$('#quantity option:selected').text(obj.quantity);
 	$('#wishListSubmit').val('Update wishList');
 	$('#wishListNotify').css('opacity', 1);
 }
+
+
