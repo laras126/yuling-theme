652261fe41a94cc5134942a168887e01855bee24
laras126 <laras126@gmail.com>
2015-12-18 11:25:23 -0500

    add context for project sidebar titles

--- sidebar-projects.php
+++ sidebar-projects.php
@@ -9,6 +9,11 @@
 
 $data['sidebar'] = Timber::get_widgets('sidebar_projects');
 $data['cats'] = Timber::get_terms('category');
+$data['industries'] = Timber::get_terms('industry');
+
+$data['projects_industry_list_title'] = get_field('projects_industry_list_title', 'options');
+$data['projects_category_list_title'] = get_field('projects_category_list_title', 'options');
+
 
 Timber::render(array('sidebars/sidebar-projects.twig'), $data);
 
