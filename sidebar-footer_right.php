9f6ad8772b066de654b8cd276e83aea9c46a7ffd
laras126 <laras126@gmail.com>
2015-12-19 10:23:02 -0500

    add field for mail chimp subscribe text, clean up markup, add placeholder

--- sidebar-footer_right.php
+++ sidebar-footer_right.php
@@ -15,4 +15,6 @@ $data['twitter'] = get_field('twitter', 'options');
 $data['facebook'] = get_field('facebook', 'options');
 $data['instagram'] = get_field('instagram', 'options');
 
+$data['subscribe_to_mailchimp'] = get_field('subscribe_to_mailchimp', 'options');
+
 Timber::render(array('sidebars/sidebar-footer_right.twig'), $data);
