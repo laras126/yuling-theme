9dbc6c715ed4a23e348a8616b4c6b4045a3bb638
laras126 <laras126@gmail.com>
2016-01-28 15:23:32 -0500

    add pinterest prompt to context

--- page-collections.php
+++ page-collections.php
@@ -9,4 +9,5 @@ $post = new TimberPost();
 $context['post'] = $post;
 $context['terms'] = Timber::get_terms('collection');
 $context['cover_image'] = get_field('collections_cover_image', 'options');
+$context['pinterest_prompt'] = get_field('pinterest_prompt', 'options');
 Timber::render('pages/page-collections.twig', $context);
